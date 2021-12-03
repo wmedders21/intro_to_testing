3711ebb1b5b0d4c6a2429e0d3ae6bf5a853087e9
Dani Coleman <danicoleman00@gmail.com>
2021-12-02 17:18:22 -0700

    Adjust the ends

--- spec/student_spec.rb
+++ spec/student_spec.rb
@@ -4,10 +4,10 @@
 describe Student do
   describe '#initialize' do
     it 'is an instance of student' do
-      # write the code to initialize a new student object 
+      # write the code to initialize a new student object
     end
+    # test it has a name
+    # test it has cookies
+    # test it can add cookies
   end
-  # test it has a name
-  # test it has cookies
-  # test it can add cookies
 end
