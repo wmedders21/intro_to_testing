eee98b9517f89e098e09d91e19ae1ee9e46b0f33
Will Medders <wmedders@gmail.com>
2022-02-01 18:13:31 -0500

    complete student and student_spec files

---
+++ lib/student.rb
@@ -0,0 +1,14 @@
+class Student
+
+  attr_reader :name, :cookies
+
+  def initialize(name)
+    @name = name
+    @cookies = []
+  end
+
+  def add_cookie(cookie)
+    @cookies << cookie
+  end
+
+end
--- spec/student_spec.rb
+++ spec/student_spec.rb
@@ -1,13 +1,42 @@
 # Add the 2 requires you will need here
-
+require 'rspec'
+require '../lib/student.rb'
 
 describe Student do
   describe '#initialize' do
-    it 'is an instance of student' do
+    it 'creates an instance of student class' do
+      student = Student.new('Penelope')
+      expect(student).to be_an_instance_of Student
       # write the code to initialize a new student object
     end
-    # test it has a name
-    # test it has cookies
-    # test it can add cookies
   end
+    # test it has a name
+  describe '#name' do
+    it 'test it has a name' do
+      student = Student.new("John")
+      expect(student.name).to eq("John")
+    end
+  end
+
+  describe '#cookies' do
+    it 'test it has cookies' do
+      student = Student.new("Picklebrain")
+      expect(student.cookies).to eq([])
+    end
+  end
+
+  describe '#add_cookie' do
+    it 'test it can add cookies' do
+      student = Student.new('Billy')
+      student.add_cookie("Chocolate chunk")
+      student.add_cookie("Snickerdoodle")
+      # require 'pry'; binding.pry
+      expect(student.cookies).to eq(["Chocolate chunk", "Snickerdoodle"])
+    end
+  end
+
+  describe
 end
+    # test it has cookies
+
+    # test it can add cookies
